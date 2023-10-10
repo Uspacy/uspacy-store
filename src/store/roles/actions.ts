229f13f7d35c44aa4ce71acb75d2fe3826953ba7
Alexander <53402125+RealSolomon@users.noreply.github.com>
2023-10-11 00:08:42 +0300

    [1690] feature: implement company (#15)

--- src/store/roles/actions.ts
+++ src/store/roles/actions.ts
@@ -1,11 +1,71 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { uspacySdk } from '@uspacy/sdk';
 
-export const fetchRoles = createAsyncThunk('users/fetchRoles', async (_, { rejectWithValue }) => {
+import { IDepartment } from './types';
+
+export const fetchRoles = createAsyncThunk('roles/fetchRoles', async (_, { rejectWithValue }) => {
 	try {
 		const res = await uspacySdk.rolesService.getRoles();
 		return res.data;
 	} catch (e) {
 		return rejectWithValue(e);
+	}
+});
+
+export const fetchPermissions = createAsyncThunk('roles/fetchPermissions', async (_, { rejectWithValue }) => {
+	try {
+		const res = await uspacySdk.rolesService.getPermissions();
+		return res.data;
+	} catch (e) {
+		return rejectWithValue(e);
+	}
+});
+
+export const fetchRole = createAsyncThunk('roles/fetchRole', async (id: string, thunkAPI) => {
+	try {
+		const res = await uspacySdk.rolesService.getRole(id);
+		return res.data;
+	} catch (e) {
+		return thunkAPI.rejectWithValue('Failure');
+	}
+});
+
+export const updateRole = createAsyncThunk(
+	'roles/updateRole',
+	async (
+		{
+			id,
+			title,
+			usersIds,
+			departmentList,
+			permissions,
+		}: { id: string; title?: string; usersIds?: string[]; departmentList?: IDepartment[]; permissions?: string[] },
+		thunkAPI,
+	) => {
+		try {
+			const res = await uspacySdk.rolesService.updateRole({ id, title, usersIds, departmentList, permissions });
+			return res.data;
+		} catch (e) {
+			return thunkAPI.rejectWithValue(e);
+		}
+	},
+);
+
+export const createRole = createAsyncThunk('roles/createRole', async ({ title, permissions }: { title: string; permissions: string[] }, thunkAPI) => {
+	try {
+		const res = await uspacySdk.rolesService.createRole({ title, permissions });
+		return res.data;
+	} catch (e) {
+		return thunkAPI.rejectWithValue(e);
+	}
+});
+
+export const deleteRole = createAsyncThunk('roles/deleteRole', async (id: string, thunkAPI) => {
+	try {
+		await uspacySdk.rolesService.deleteRole(id);
+
+		return id;
+	} catch (e) {
+		return thunkAPI.rejectWithValue('Failure');
 	}
 });
