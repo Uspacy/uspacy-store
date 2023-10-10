e5f84ef8223bcdff0b7e80be1409dc7bd0f666c2
Alexander Solomahin <alexsolomahin377@gmail.com>
2023-10-10 14:28:25 +0300

    add roles

--- src/store/roles/types.ts
+++ src/store/roles/types.ts
@@ -1,14 +1,28 @@
 import { IErrorsAxiosResponse } from '@uspacy/sdk/lib/models/errors';
-import { IRole } from '@uspacy/sdk/lib/models/roles';
+import { IPermissions, IRole } from '@uspacy/sdk/lib/models/roles';
 import { IUser } from '@uspacy/sdk/lib/models/user';
 
 export interface IState {
 	roles: IRole[];
+	permissions: IPermissions;
+	createPermissions: IPermissions;
 	loadingRoles: boolean;
 	errorLoadingRoles: IErrorsAxiosResponse;
+	filterCounter?: number;
+	selectedRole: string;
 	activeRole?: string;
 	roleData: {
 		name: string;
 		userList: IUser[];
 	};
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	permissionsControllerView: any[];
+	modalActiveType: string;
+	createPermissionsModal: IPermissions;
+}
+
+export interface IDepartment {
+	id: string;
+	name: string;
+	dateUpdate: number;
 }
