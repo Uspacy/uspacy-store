d2a2c40f4a18b75291b6d0310d4dc2c2a47ac33c
Andrii Malyshev <56361788+AndreyMalyshev147@users.noreply.github.com>
2023-11-24 19:38:29 +0200

    [1960] features: new groups kanban (#55)

--- src/store/tasksStages/actions.ts
+++ src/store/tasksStages/actions.ts
@@ -1,9 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { uspacySdk } from '@uspacy/sdk';
 
-export const fetchStages = createAsyncThunk('stages/fetchStages', async (_, thunkAPI) => {
+export const fetchStages = createAsyncThunk('stages/fetchStages', async (groupId: number, thunkAPI) => {
 	try {
-		const res = await uspacySdk.tasksStagesService.getTasksStages();
+		const res = await uspacySdk.tasksStagesService.getTasksStages(groupId);
 		return res.data;
 	} catch (e) {
 		return thunkAPI.rejectWithValue('Failure');
