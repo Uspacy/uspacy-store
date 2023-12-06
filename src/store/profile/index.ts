04a83baa69df7251e4f7a0c35b86944798ef7f4c
MysikM <mishamysik@gmail.com>
2023-12-06 09:25:08 +0200

    update

--- src/store/profile/index.ts
+++ src/store/profile/index.ts
@@ -45,7 +45,11 @@ export const profileSlice = createSlice({
 			state.loading = action.payload;
 		},
 		addToRequisites(state, action: PayloadAction<IRequisite>) {
-			state.requisites = [action.payload, ...state.requisites];
+			state.requisites = [action.payload, ...state.requisites].map((req) => {
+				const newBase = action.payload.is_basic;
+				if (req?.id === action.payload.id) return { ...req, ...action.payload };
+				return { ...req, is_basic: newBase ? false : req.is_basic };
+			});
 		},
 		updateRequisites(state, action: PayloadAction<IRequisite>) {
 			state.requisites = state.requisites.map((req) => {
