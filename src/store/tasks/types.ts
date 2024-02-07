a1ff29135ce65ea86d8f12290aa123481c7858a1
Andrii Malyshev <56361788+AndreyMalyshev147@users.noreply.github.com>
2024-02-07 17:58:24 +0200

    [2355] feature: new major update for templates (#78)

--- src/store/tasks/types.ts
+++ src/store/tasks/types.ts
@@ -1,19 +1,25 @@
 import { IErrorsAxiosResponse } from '@uspacy/sdk/lib/models/errors';
 import { IFields } from '@uspacy/sdk/lib/models/field';
-import { IFilterTasks, IMeta, ITask, ITasks } from '@uspacy/sdk/lib/models/tasks';
+import { IFilterTasks, IMeta, ITask, ITasks, taskType } from '@uspacy/sdk/lib/models/tasks';
 
 export interface ITaskCardActions {
+	type: taskType;
 	mode: 'view' | 'add' | 'edit';
+}
+
+export interface IDeleteTaskPayload {
+	id: string;
+	type: taskType;
 }
 
 export interface IState {
 	tasks: ITasks;
-	regularTasks: ITasks;
 	subtasks: ITasks;
 	allSubtasks: ITask[];
 	task: ITask;
+	recurringTemplate: ITask;
 	parentTask: ITask;
-	template: ITask;
+	taskFromTemplate: ITask;
 	addedTask: ITask;
 	addedToKanbanTask: ITask;
 	changeTask: ITask;
@@ -24,37 +30,36 @@ export interface IState {
 	filters: IFilterTasks;
 	regularFilter: IFilterTasks;
 	taskFields: IFields;
-	tasksCardPermissions: ITaskCardActions;
 	isEditMode: boolean;
 	loadingTasks: boolean;
-	loadingRegularTasks: boolean;
 	loadingSubtasks: boolean;
 	loadingTask: boolean;
+	loadingRecurringTemplate: boolean;
 	loadingParentTask: boolean;
-	loadingTemplate: boolean;
-	loadingAddingTask: boolean;
-	loadingEditingTask: boolean;
+	loadingCreatingTask: boolean;
+	loadingUpdatingTask: boolean;
 	loadingDeletingTask: boolean;
 	loadingStatusesTask: boolean;
 	loadingTaskFields: boolean;
 	errorLoadingTasks: IErrorsAxiosResponse;
-	errorLoadingSchedulerTasks: IErrorsAxiosResponse;
 	errorLoadingSubtasks: IErrorsAxiosResponse;
 	errorLoadingTask: IErrorsAxiosResponse;
+	errorLoadingRecurringTemplate: IErrorsAxiosResponse;
 	errorLoadingParentTask: IErrorsAxiosResponse;
-	errorLoadingTemplate: IErrorsAxiosResponse;
-	errorLoadingAddingTask: IErrorsAxiosResponse;
-	errorLoadingEditingTask: IErrorsAxiosResponse;
+	errorLoadingCreatingTask: IErrorsAxiosResponse;
+	errorLoadingUpdatingTask: IErrorsAxiosResponse;
 	errorLoadingDeletingTask: IErrorsAxiosResponse;
 	errorLoadingStatusesTask: IErrorsAxiosResponse;
 	errorLoadingTaskFields: IErrorsAxiosResponse;
 	meta: IMeta;
-	regularTasksMeta: IMeta;
 	popupLinks: ITask[];
 	isSubtasks: boolean;
 	isCopyingTask: boolean;
+	isTaskFromTemplate: boolean;
 	isKanban: boolean;
 	isTable: boolean;
 	taskStatus: string;
 	isRegularSection: boolean;
+	tasksCardPermissions: ITaskCardActions;
+	tasksServiceType: taskType;
 }
