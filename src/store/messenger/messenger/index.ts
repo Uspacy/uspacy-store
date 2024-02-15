6ad80bcf3ebacf98e6bf8da3c86c63f220bc6e8f
Alexander Solomahin <alexsolomahin377@gmail.com>
2024-02-15 21:19:09 +0200

    fix build

--- src/store/messenger/messenger/index.ts
+++ src/store/messenger/messenger/index.ts
@@ -526,7 +526,7 @@ export const chatSlice = createSlice({
 					chatId: action.meta.arg.chatId,
 					items: [],
 					loading: true,
-					message: '',
+					draftMessage: '',
 				});
 			} else {
 				state.messages = state.messages.map((group) => {
@@ -559,7 +559,7 @@ export const chatSlice = createSlice({
 			// fix when we go to message in first opened chat
 			if (isFirstOpenedChat) {
 				state.messages.push({
-					message: '',
+					draftMessage: '',
 					chatId: action.payload.items[0]?.chatId,
 					items: prepereMessages(action.payload.items, action.payload.profile),
 					loading: false,
