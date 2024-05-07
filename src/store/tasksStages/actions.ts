786d17373c14366cc9ffbe33126d0a7a7055ef01
AndreyMalyshev147 <andrey.malyshev.97@gmail.com>
2024-05-07 12:38:10 +0300

    Updated sdk version

--- src/store/tasksStages/actions.ts
+++ src/store/tasksStages/actions.ts
@@ -1,10 +1,20 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { uspacySdk } from '@uspacy/sdk';
 
+import { IMoveTaskPayload } from './types';
+
 export const fetchStages = createAsyncThunk('stages/fetchStages', async (groupId: number, thunkAPI) => {
 	try {
 		const res = await uspacySdk.tasksStagesService.getTasksStages(groupId);
 		return res.data;
+	} catch (e) {
+		return thunkAPI.rejectWithValue('Failure');
+	}
+});
+
+export const moveTask = createAsyncThunk('stages/moveTask', async ({ cardId, stageId, prevCardId }: IMoveTaskPayload, thunkAPI) => {
+	try {
+		return await uspacySdk.tasksStagesService.moveTask(cardId, stageId, prevCardId);
 	} catch (e) {
 		return thunkAPI.rejectWithValue('Failure');
 	}
