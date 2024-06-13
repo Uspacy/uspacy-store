8de4463e4cb39f86c5130beab3ca58a4c21d039b
MysikM <mishamysik@gmail.com>
2024-06-13 14:56:14 +0300

    check empty

--- src/helpers/filterFieldsArrs.ts
+++ src/helpers/filterFieldsArrs.ts
@@ -220,6 +220,10 @@ export const getFilterParams = (filters: IFilter, fields: IField[], isKanban = f
 				return { ...acc, [key]: isArray(value) ? value : [value] };
 			}
 
-			return { ...acc, [key]: value };
+			if (!!findField?.type) {
+				return { ...acc, [key]: value };
+			}
+
+			return acc;
 		}, {});
 };
