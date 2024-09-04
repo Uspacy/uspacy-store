f0c2b5503ef2e350a6948d28e027e459c1e859be
Stas Raranetskyi <rar.stas@gmail.com>
2024-09-04 13:11:41 +0100

    [feat] add settings reducer

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,7 @@
 import { IField } from '@uspacy/sdk/lib/models/field';
 
 export const API_PREFIX_FILES = '/files/v1';
+export const API_PREFIX_SETTINGS = '/settings/v1';
 
 export const idColumn: IField = {
 	name: 'number_',
