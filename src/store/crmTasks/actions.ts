a12509cf753c3658875e2af89dd781e8f72d3f39
Andrii Malyshev <56361788+AndreyMalyshev147@users.noreply.github.com>
2024-10-01 09:24:39 +0300

    [2416] feature: oauth activities in the calendar/store (#143)

--- src/store/crmTasks/actions.ts
+++ src/store/crmTasks/actions.ts
@@ -4,6 +4,7 @@ import { ITaskFilters } from '@uspacy/sdk/lib/models/crm-filters';
 import { IMassActions } from '@uspacy/sdk/lib/models/crm-mass-actions';
 import { ITask } from '@uspacy/sdk/lib/models/crm-tasks';
 import { IField } from '@uspacy/sdk/lib/models/field';
+import { ICalendarSettings, ISyncSettings } from '@uspacy/sdk/lib/services/CrmTasksService/calendars-settings.dto';
 
 import { getFilterParams } from './../../helpers/filterFieldsArrs';
 import { makeURIParams } from './../../helpers/makeURIParams';
@@ -114,3 +115,87 @@ export const massTasksEditing = createAsyncThunk(
 		}
 	},
 );
+
+export const getOAuth2CalendarRedirectUrl = createAsyncThunk('crmTasks/getOAuth2CalendarRedirectUrl', async (_, { rejectWithValue }) => {
+	try {
+		const res = await uspacySdk?.crmTasksService?.getOAuth2CalendarRedirectUrl();
+		return res?.data;
+	} catch (e) {
+		return rejectWithValue(e);
+	}
+});
+
+export const getCalendarsAccounts = createAsyncThunk('crmTasks/getCalendarsAccounts', async (_, { rejectWithValue }) => {
+	try {
+		const res = await uspacySdk?.crmTasksService?.getCalendarsAccounts();
+		return res?.data;
+	} catch (e) {
+		return rejectWithValue(e);
+	}
+});
+
+export const deleteCalendarsAccount = createAsyncThunk('crmTasks/deleteCalendarsAccount', async (id: number, { rejectWithValue }) => {
+	try {
+		await uspacySdk?.crmTasksService?.deleteCalendarsAccount();
+		return id;
+	} catch (e) {
+		return rejectWithValue(e);
+	}
+});
+
+export const getGoogleCalendars = createAsyncThunk('crmTasks/getGoogleCalendars', async (_, { rejectWithValue }) => {
+	try {
+		const res = await uspacySdk?.crmTasksService?.getGoogleCalendars();
+		return res?.data;
+	} catch (e) {
+		return rejectWithValue(e);
+	}
+});
+
+export const saveCalendarSettings = createAsyncThunk('crmTasks/saveCalendarSettings', async (body: ICalendarSettings, { rejectWithValue }) => {
+	try {
+		const res = await uspacySdk?.crmTasksService?.saveCalendarSettings(body);
+		return res?.data;
+	} catch (e) {
+		return rejectWithValue(e);
+	}
+});
+
+export const startInitialGoogleCalendarsSync = createAsyncThunk(
+	'crmTasks/startInitialGoogleCalendarsSync',
+	async ({ body, id }: { body: ISyncSettings; id: number }, { rejectWithValue }) => {
+		try {
+			await uspacySdk?.crmTasksService?.startInitialGoogleCalendarsSync(body);
+			return id;
+		} catch (e) {
+			return rejectWithValue(e);
+		}
+	},
+);
+
+export const startCalendarsSync = createAsyncThunk('crmTasks/startCalendarsSync', async (id: number, { rejectWithValue }) => {
+	try {
+		await uspacySdk?.crmTasksService?.startCalendarsSync();
+		return id;
+	} catch (e) {
+		return rejectWithValue(e);
+	}
+});
+
+export const stopGoogleCalendarsSync = createAsyncThunk('crmTasks/stopGoogleCalendarsSync', async (id: number, { rejectWithValue }) => {
+	try {
+		await uspacySdk?.crmTasksService?.stopGoogleCalendarsSync();
+		return id;
+	} catch (e) {
+		return rejectWithValue(e);
+	}
+});
+
+export const activateGoogleCalendarsSync = createAsyncThunk('crmTasks/activateGoogleCalendarsSync', async (id: number, { rejectWithValue }) => {
+	try {
+		await uspacySdk?.crmTasksService?.activateGoogleCalendarsSync(id);
+		return id;
+	} catch (e) {
+		return rejectWithValue(e);
+	}
+});
