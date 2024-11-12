3d35ca852073ac812db57966087371bf91027592
Alexander <alexsolomahin377@gmail.com>
2024-11-12 13:48:09 +0200

    change reducer

--- src/store/announcers/index.ts
+++ src/store/announcers/index.ts
@@ -1,9 +1,33 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { IAnnouncers } from '@uspacy/sdk/lib/services/AnnouncersService/dto/announcers-dto';
 
+import { fetchAnnouncers } from './actions';
 import { IState } from './types';
 
 const initialState: IState = {
+	loadingAnnouncers: false,
+	errorLoading: false,
 	isBannerExists: false,
+	announcers: {
+		data: {
+			data: {
+				id: 0,
+				attributes: {
+					notification: null,
+					widget: [],
+					banner: null,
+				},
+			},
+			meta: {
+				pagination: {
+					page: 0,
+					pageSize: 0,
+					pageCount: 0,
+					total: 0,
+				},
+			},
+		},
+	},
 };
 
 export const announcersSlice = createSlice({
@@ -14,7 +38,21 @@ export const announcersSlice = createSlice({
 			state.isBannerExists = action.payload;
 		},
 	},
-	extraReducers: {},
+	extraReducers: {
+		[fetchAnnouncers.fulfilled.type]: (state, action: PayloadAction<IAnnouncers>) => {
+			state.loadingAnnouncers = false;
+			state.errorLoading = null;
+			state.announcers = action.payload;
+		},
+		[fetchAnnouncers.pending.type]: (state) => {
+			state.loadingAnnouncers = true;
+			state.errorLoading = null;
+		},
+		[fetchAnnouncers.rejected.type]: (state) => {
+			state.loadingAnnouncers = false;
+			state.errorLoading = true;
+		},
+	},
 });
 
 export const { setBannerExists } = announcersSlice.actions;
--- src/store/announcers/types.ts
+++ src/store/announcers/types.ts
@@ -1,3 +1,8 @@
+import { IAnnouncers } from '@uspacy/sdk/lib/services/AnnouncersService/dto/announcers-dto';
+
 export interface IState {
+	loadingAnnouncers: boolean;
+	errorLoading: boolean;
+	announcers: IAnnouncers;
 	isBannerExists: boolean;
 }
