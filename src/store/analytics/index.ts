b52047ac8142dd4da903435e02610f4d3489ba58
MysikM <mishamysik@gmail.com>
2024-11-18 21:46:51 +0200

    add mock

--- src/store/analytics/index.ts
+++ src/store/analytics/index.ts
@@ -2,8 +2,33 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IAnalyticReport, IAnalyticReportList } from '@uspacy/sdk/lib/models/analytics';
 import { IErrorsAxiosResponse } from '@uspacy/sdk/lib/models/errors';
 
-import { createReport, deleteReport, getAnalyticsReportList, updateReport } from './actions';
+import { createReport, deleteReport, getAnalyticsReportList, getReport, updateReport } from './actions';
 import { IState } from './types';
+
+const mock: IAnalyticReport = {
+	id: 4,
+	title: 'toster 123123',
+	chart_type: 'column',
+	description:
+		'<p class="PlaygroundEditorTheme__paragraph" dir="ltr"><span style="white-space: pre-wrap;">super </span><b><strong class="PlaygroundEditorTheme__textBold" style="white-space: pre-wrap;">tost s</strong></b></p>',
+	entity_table_name: 'activity',
+	logical_operator: 'OR',
+	filter: {
+		main: [
+			{ field_code: 'type', value: ['task', 'call', 'meeting', 'chat'] },
+			{ field_code: 'status', value: ['planned', 'done', 'cancelled'] },
+			{ field_code: 'priority', value: ['veryHigh', 'neutral', 'notHigh', 'high'] },
+			{ field_code: 'description', value: ['test 1234'] },
+		],
+		group_by: '',
+		view_by: { timeframe: 'day', value: 'responsible_id' },
+		measure_for: 'count',
+		additional: { is_view_percent: true, is_view_value: true },
+	},
+	panel_ids: [],
+	created_at: 1731851087,
+	owner_id: 1,
+};
 
 const initialState = {
 	reports: {
@@ -13,7 +38,7 @@ const initialState = {
 			page: 1,
 		},
 	},
-	report: {},
+	report: mock,
 	loadingReports: false,
 	loadingReport: false,
 	errorLoadingReports: null,
@@ -22,13 +47,16 @@ const initialState = {
 const analyticsReducer = createSlice({
 	name: 'analytics',
 	initialState,
-	reducers: {},
+	reducers: {
+		clearAllReport: (state) => {
+			state.reports = initialState.reports;
+		},
+	},
 	extraReducers: {
 		[getAnalyticsReportList.fulfilled.type]: (state, action: PayloadAction<IAnalyticReportList>) => {
 			state.loadingReports = false;
 			state.errorLoadingReports = null;
-			state.reports.data = action.payload.data;
-			state.reports.meta = action.payload.meta;
+			state.reports = { ...state.reports, data: [...state.reports.data, ...action.payload.data], meta: state.reports.meta };
 		},
 		[getAnalyticsReportList.pending.type]: (state) => {
 			state.loadingReports = true;
@@ -38,11 +66,27 @@ const analyticsReducer = createSlice({
 			state.loadingReports = false;
 			state.errorLoadingReports = action.payload;
 		},
+		[getReport.fulfilled.type]: (state, action: PayloadAction<IAnalyticReport>) => {
+			state.loadingReport = false;
+			state.errorLoadingReports = null;
+			state.report = action.payload;
+		},
+		[getReport.pending.type]: (state) => {
+			state.loadingReport = true;
+			state.errorLoadingReports = null;
+		},
+		[getReport.rejected.type]: (state, action: PayloadAction<IErrorsAxiosResponse>) => {
+			state.loadingReport = false;
+			state.errorLoadingReports = action.payload;
+		},
 		[createReport.fulfilled.type]: (state, action: PayloadAction<IAnalyticReport>) => {
 			state.loadingReport = false;
 			state.errorLoadingReports = null;
-			state.reports.data = [action.payload, ...state.reports.data];
-			state.reports.meta.total = state.reports.meta.total + 1;
+			state.reports = {
+				...state.reports,
+				data: [action.payload, ...state.reports.data],
+				meta: { ...state.reports.meta, total: state.reports.meta.total + 1 },
+			};
 		},
 		[createReport.pending.type]: (state) => {
 			state.loadingReport = true;
@@ -55,8 +99,7 @@ const analyticsReducer = createSlice({
 		[updateReport.fulfilled.type]: (state, action: PayloadAction<IAnalyticReport>) => {
 			state.loadingReport = false;
 			state.errorLoadingReports = null;
-			state.reports.data = state.reports.data.map((it) => (it.id === action.payload.id ? action.payload : it));
-			state.reports.meta.total = state.reports.meta.total + 1;
+			state.reports = { ...state.reports, data: state.reports.data.map((it) => (it.id === action.payload.id ? action.payload : it)) };
 		},
 		[updateReport.pending.type]: (state) => {
 			state.loadingReport = true;
@@ -69,8 +112,11 @@ const analyticsReducer = createSlice({
 		[deleteReport.fulfilled.type]: (state, action: PayloadAction<number>) => {
 			state.loadingReport = false;
 			state.errorLoadingReports = null;
-			state.reports.data = state.reports.data.filter((it) => it.id !== action.payload);
-			state.reports.meta.total = state.reports.meta.total - 1;
+			state.reports = {
+				...state.reports,
+				data: state.reports.data.filter((it) => it.id !== action.payload),
+				meta: { ...state.reports.meta, total: state.reports.meta.total - 1 },
+			};
 		},
 		[deleteReport.pending.type]: (state) => {
 			state.loadingReport = true;
