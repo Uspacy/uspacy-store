52300cee00c97d315cff4e9959f3c3d4b648774c
AndreyMalyshev147 <andrey.malyshev.97@gmail.com>
2025-01-20 14:42:16 +0200

    Fixed store for stage branch

--- src/hooks/redux.ts
+++ src/hooks/redux.ts
@@ -8,7 +8,7 @@ export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export const useMemoizedAppSelector = <T>(selector: (state: RootState) => T) => {
-	const dataRef = useRef<T>();
+	const dataRef = useRef<T>(null);
 	return useAppSelector((state) => {
 		const data = selector(state);
 		if (isEqual(dataRef.current, data)) {
