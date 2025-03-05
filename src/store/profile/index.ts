c22a65a81f23b20a8066e73e22d8ecae63a5ef31
MysikM <mishamysik@gmail.com>
2025-03-05 13:45:55 +0200

    up v

--- src/store/profile/index.ts
+++ src/store/profile/index.ts
@@ -93,8 +93,8 @@ export const profileSlice = createSlice({
 		[fetchProfileFields.rejected.type]: (state) => {
 			state.loadingFields.get = false;
 		},
-		[updateProfileField.pending.type]: (state, action: PayloadAction<IField, string, { arg: IField }>) => {
-			const updateField = action.meta.arg;
+		[updateProfileField.pending.type]: (state, action: PayloadAction<IField, string, { arg: { data: IField } }>) => {
+			const updateField = action.meta.arg.data;
 			state.fields = state.fields.map((field) => {
 				if (field.code === updateField.code) {
 					return { ...updateField, values: updateField?.values || field?.values };
