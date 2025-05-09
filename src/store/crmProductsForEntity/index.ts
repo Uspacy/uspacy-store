9706bb54d5fcb002cb594d2e19e75775abaff118
MysikM <mishamysik@gmail.com>
2025-05-09 16:49:34 +0300

    remove logic

--- src/store/crmProductsForEntity/index.ts
+++ src/store/crmProductsForEntity/index.ts
@@ -196,7 +196,7 @@ const productsForEntityReducer = createSlice({
 			state.loading = false;
 			state.errorMessage = '';
 			const addedItems = action.payload.data.map((it, idx) => {
-				return { ...it, product: action?.meta?.arg?.[idx]?.product };
+				return { ...it };
 			});
 
 			state.productsWithInfoForEntity.list_products = [
