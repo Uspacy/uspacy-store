e8bc0ccc5baa4034e806518ff699195c250e8fb3
AndreyMalyshev147 <andrey.malyshev.97@gmail.com>
2025-07-15 14:42:53 +0300

    Upd checks for transformKeysToCaseByType

--- src/helpers/objectsUtilities.ts
+++ src/helpers/objectsUtilities.ts
@@ -1,13 +1,13 @@
 export const transformKeysToCaseByType = (object: object, type: 'snake' | 'camel') => {
 	if (type === 'snake') {
-		return Object.keys(object).reduce((acc, key) => {
+		return Object.keys(object || {}).reduce((acc, key) => {
 			const snakeCaseKey = key.replace(/([A-Z])/g, '_$1').toLowerCase();
 			acc[snakeCaseKey] = object[key];
 			return acc;
 		}, {});
 	}
 
-	return Object.keys(object).reduce((acc, key) => {
+	return Object.keys(object || {}).reduce((acc, key) => {
 		const camelCaseKey = key.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
 		acc[camelCaseKey] = object[key];
 		return acc;
