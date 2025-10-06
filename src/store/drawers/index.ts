1a59466373ba60bac07700a31b20164b8a791814
MysikM <mishamysik@gmail.com>
2025-10-06 16:08:15 +0300

    add drawers

---
+++ src/store/drawers/index.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+import { IDrawerNavItem, IState } from './types';
+
+const initialState: IState = {
+	drawers: [],
+	activeId: null,
+};
+
+export const multiDrawersSlice = createSlice({
+	name: 'drawers',
+	initialState,
+	reducers: {
+		addItem(state, action: PayloadAction<IDrawerNavItem>) {
+			if (!state.drawers?.some((it) => it.entityCode === action.payload.entityCode && it.entityId === action.payload.entityId)) {
+				state.drawers = [action.payload, ...(state?.drawers || [])];
+			}
+			state.activeId = `${action?.payload.entityCode}-${action.payload.entityId}`;
+		},
+		removeItem(state, action: PayloadAction<IDrawerNavItem>) {
+			state.drawers = state.drawers?.filter((it) => it.entityCode !== action?.payload?.entityCode && it.entityId !== action?.payload?.entityId);
+			state.activeId = !!state.drawers?.[0]?.entityCode ? `${state.drawers?.[0]?.entityCode}-${state.drawers?.[0]?.entityId}` : null;
+		},
+		closeAll(state) {
+			state.drawers = initialState.drawers;
+			state.activeId = initialState.activeId;
+		},
+	},
+	extraReducers: {},
+});
+
+export const { addItem, removeItem, closeAll } = multiDrawersSlice.actions;
+
+export default multiDrawersSlice.reducer;
---
+++ src/store/drawers/types.ts
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+
+export interface IDrawerNavItem {
+	id: string;
+	label: string;
+	icon?: ReactNode;
+	render: ReactNode | (() => ReactNode);
+	disabled?: boolean;
+	entityCode?: string;
+	entityId?: string;
+	service?: 'task' | 'crm';
+}
+
+export interface IState {
+	activeId: string;
+	drawers: IDrawerNavItem[];
+}
