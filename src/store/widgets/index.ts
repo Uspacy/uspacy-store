8ed9588660809f9d9cb6f2f395c9cbb25fc0f9ed
Вітас Пірожко <vitaspirozko@MacBookAir.Dlink>
2026-02-26 18:58:46 +0200

    move widget data in to store

--- src/store/widgets/index.ts
+++ src/store/widgets/index.ts
@@ -6,52 +6,77 @@ import { createWidget, deleteWidget, getWidgets, updateWidget } from './actions'
 import { IState } from './types';
 
 const initialState: IState = {
-	widgets: {
-		data: [],
-		meta: {
-			currentPage: 1,
-			from: 0,
-			lastPage: 1,
-			perPage: 0,
-			to: 0,
-			total: 0,
+	widgetData: {
+		name: '',
+		nameExternalLine: '',
+		settings: {
+			icon: null,
+			welcomeMessage: '',
+			iconColor: '',
+			backgroundColor: '',
+			operatorName: '',
+			operatorAvatar: '',
 		},
-		loading: false,
 	},
+	widgetsList: [],
+	meta: {
+		currentPage: 1,
+		from: 0,
+		lastPage: 1,
+		perPage: 0,
+		to: 0,
+		total: 0,
+	},
+	loading: false,
 };
 
 export const widgetsSlice = createSlice({
 	name: 'widgets',
 	initialState,
-	reducers: {},
+	reducers: {
+		setWidgetData: (state, action: PayloadAction<ICreateWidgetData>) => {
+			state.widgetData = action.payload;
+		},
+		updateWidgetData: (
+			state,
+			action: PayloadAction<{ type: 'body' | 'settings'; data: Partial<ICreateWidgetData>['settings'] | Partial<ICreateWidgetData> }>,
+		) => {
+			const { type, data } = action.payload;
+			if (type === 'body') {
+				state.widgetData = { ...state.widgetData, ...data };
+			} else if (type === 'settings') {
+				state.widgetData.settings = { ...state.widgetData.settings, ...data };
+			}
+		},
+	},
 	extraReducers: {
 		[createWidget.pending.type]: (state) => {
-			state.widgets.loading = true;
+			state.loading = true;
 		},
 		[createWidget.rejected.type]: (state) => {
-			state.widgets.loading = false;
+			state.loading = false;
 		},
 		[createWidget.fulfilled.type]: (state, action: PayloadAction<ICreateWidgetData>) => {
-			state.widgets.data = [...state.widgets.data, action.payload];
-			state.widgets.meta.total = state.widgets.meta.total + 1;
-			state.widgets.loading = false;
+			state.widgetsList = [...state.widgetsList, action.payload];
+			state.meta.total = state.meta.total + 1;
+			state.loading = false;
 		},
 		[getWidgets.pending.type]: (state) => {
-			state.widgets.loading = true;
+			state.loading = true;
 		},
 		[getWidgets.rejected.type]: (state) => {
-			state.widgets.loading = false;
+			state.loading = false;
 		},
 		[getWidgets.fulfilled.type]: (state, action: PayloadAction<{ data: ICreateWidgetData[]; meta: IMeta }>) => {
-			state.widgets.data = action.payload.data;
-			state.widgets.meta = action.payload.meta;
-			state.widgets.loading = false;
+			state.widgetsList = action.payload.data;
+			state.meta = action.payload.meta;
+			state.loading = false;
 		},
 		[deleteWidget.fulfilled.type]: (state, action: PayloadAction<ICreateWidgetData>) => {
-			state.widgets.data = state.widgets.data.filter((it) => it.id !== action.payload.id);
+			state.widgetsList = state.widgetsList.filter((it) => it.id !== action.payload.id);
 		},
 		[updateWidget.fulfilled.type]: (state, action: PayloadAction<ICreateWidgetData>) => {
-			state.widgets.data = state.widgets.data.map((it) => {
+			state.widgetsList = state.widgetsList.map((it) => {
 				if (it.id === action.payload.id) return action.payload;
 				return it;
 			});
@@ -59,6 +84,6 @@ export const widgetsSlice = createSlice({
 	},
 });
 
-export const {} = widgetsSlice.actions;
+export const { setWidgetData, updateWidgetData } = widgetsSlice.actions;
 
 export default widgetsSlice.reducer;
--- src/store/widgets/types.ts
+++ src/store/widgets/types.ts
@@ -2,9 +2,8 @@ import { ICreateWidgetData } from '@uspacy/sdk/lib/models/messenger';
 import { IMeta } from '@uspacy/sdk/lib/models/tasks';
 
 export interface IState {
-	widgets: {
-		data: ICreateWidgetData[];
-		meta: IMeta;
-		loading: boolean;
-	};
+	widgetData: ICreateWidgetData;
+	widgetsList: ICreateWidgetData[];
+	meta: IMeta;
+	loading: boolean;
 }
