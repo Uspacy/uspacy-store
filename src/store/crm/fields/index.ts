f94df9b598ce8132105bcb5ebe9926cb3563f1fa
MysikM <mishamysik@gmail.com>
2026-07-08 12:03:09 +0300

    update

--- src/store/crm/fields/index.ts
+++ src/store/crm/fields/index.ts
@@ -28,7 +28,7 @@ const feildsReducer = createSlice({
 				data.splice(0, 0, taskField);
 			}
 			if (entityCode === 'leads') {
-				data.splice(1, 0, taskField);
+				data.splice(0, 0, taskField);
 			}
 			if (entityCode === 'calls') {
 				data.splice(0, 0, ...fieldForCalls);
